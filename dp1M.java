839e014f42d0f2d629b8f9bc56dbd944d00e876b
krishfixerra <krish.bhardwaj@fixerra.in>
2025-12-01 22:14:58 +0530

    the dp1

--- dp1M.java
+++ dp1M.java
@@ -1,23 +1,32 @@
 class Solution {
+    int dp[];
     public int climbStairs(int n) {
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
         return sol(n);
     }
     public int sol(int n)
     {
         if(n==0)
         {
-            return 0;
+            return dp[0]= 0;
         }
         if(n==1)
         {
-            return 1;
+            return dp[1]= 1;
         }
         if(n==2)
         {
-            return 2;
+            return dp[2]= 2;
         }
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+
+
         int ways=sol(n-1)+sol(n-2);
+        dp[n]=ways;
         return ways;
     }
 }
-//focus on last step either the user came from n-1 or n-2 so we add both the ways it will exceed time limit for large n
